09574ed90de914ccbaeb5ce5f3bab2f21defebc1
seamussmith <student.141504@worcesterschools.net>
2021-09-15 19:04:48 -0400

    rename variables for easier reading

--- src/main/App.java
+++ src/main/App.java
@@ -9,31 +9,33 @@ public class App
         var in = new Scanner(System.in);
         System.out.println(evalExpression(in.nextLine()));
     }
-    static double evalExpression(String exp)
+    static double evalExpression(String expression)
     {
         // Put each token in the string into a queue for easier parsing
-        var split = Arrays.asList(exp.split(" "));
+        var split = Arrays.asList(expression.split(" "));
         Queue<String> newStack = new LinkedList<String>();
         newStack.addAll(split.stream().filter(n -> n.length() != 0).toList());
 
         return processExpression(newStack);
     }
-    static double processExpression(Queue<String> exp)
+    static double processExpression(Queue<String> tokens)
     {
         // If the next token is a number, just return it
-        if (exp.peek().matches("^\\-?[1-9]\\d*(\\.\\d+)?$"))
-            return Double.parseDouble(exp.poll());
+        if (tokens.peek().matches("^\\-?[1-9]\\d*(\\.\\d+)?$"))
+            return Double.parseDouble(tokens.poll());
         
-        var op = exp.poll();
+        // Data needed to calculate expression
+        var op = tokens.poll();
         Queue<Double> nums = new LinkedList<Double>();
 
         // Get every number until the queue is empty.
         // If the next token is an operator,
         // the recursive function will just calculate that expression, and so on.
         // BECAUSE RECURSION!
-        while (!exp.isEmpty())
-            nums.add(processExpression(exp));
+        while (!tokens.isEmpty())
+            nums.add(processExpression(tokens));
         
+        // Number to start reducing on
         var start = nums.poll();
 
         // And do the arithmetic
