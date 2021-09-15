8990ef485763a36a08b6b824376ef87580399bda
seamussmith <student.141504@worcesterschools.net>
2021-09-15 18:23:56 -0400

    finished

--- src/main/App.java
+++ src/main/App.java
@@ -2,35 +2,37 @@ package main;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class App
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, World!");
+        var in = new Scanner(System.in);
+        System.out.println(evalExpression(in.nextLine()));
     }
     static double evalExpression(String exp)
     {
-        return processExpression(new Scanner(
-            Arrays.stream(exp.split(" "))
-                    .filter(n -> n.length() != 0)
-                    .reduce("", (a, b) -> a + "\n" + b)
-            )
-        );
+        var split = Arrays.asList(exp.split(" "));
+        Collections.reverse(split);
+        var newStack = new Stack<String>();
+        newStack.addAll(split.stream().filter(n -> n.length() != 0).toList());
+        return processExpression(newStack);
     }
-    static double processExpression(Scanner exp)
+    static double processExpression(Stack<String> exp)
     {
-        if (exp.hasNextDouble())
+        if (exp.peek().matches("^-?\\d+$"))
         {
-            return exp.nextDouble();
+            return Double.parseDouble(exp.pop());
         }
         
         var result = 0.0;
         var nums = new ArrayList<Double>();
-        var op = exp.next();
-        
-        while (exp.hasNextDouble())
+        var op = exp.pop();
+
+        while (!exp.isEmpty())
         {
             nums.add(processExpression(exp));
         }
@@ -40,15 +42,17 @@ public class App
         }
         else if (op.equals("-"))
         {
-            result = nums.stream().reduce(0.0, (a, b) -> a - b);
+            var start = nums.get(0) * 2;
+            result = nums.stream().reduce(start, (a, b) -> a - b);
         }
         else if (op.equals("*"))
         {
-            result = nums.stream().reduce(0.0, (a, b) -> a * b);
+            result = nums.stream().reduce(1.0, (a, b) -> a * b);
         }
         else if (op.equals("/"))
         {
-            result = nums.stream().reduce(0.0, (a, b) -> a / b);
+            var start = nums.get(0) * nums.get(0);
+            result = nums.stream().reduce(start, (a, b) -> a / b);
         }
 
         return result;
