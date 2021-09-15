4fe3a01650f4455c0a40a82a3d98da6a282afe7d
seamussmith <student.141504@worcesterschools.net>
2021-09-15 17:51:04 -0400

    Rough implmentation

--- src/main/App.java
+++ src/main/App.java
@@ -1,9 +1,56 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class App
 {
     public static void main(String[] args) 
     {
         System.out.println("Hello, World!");
+    }
+    static double evalExpression(String exp)
+    {
+        return processExpression(new Scanner(
+            Arrays.stream(exp.split(" "))
+                    .filter(n -> n.length() != 0)
+                    .reduce("", (a, b) -> a + "\n" + b)
+            )
+        );
+    }
+    static double processExpression(Scanner exp)
+    {
+        if (exp.hasNextDouble())
+        {
+            return exp.nextDouble();
+        }
+        
+        var result = 0.0;
+        var nums = new ArrayList<Double>();
+        var op = exp.next();
+        
+        while (exp.hasNextDouble())
+        {
+            nums.add(processExpression(exp));
+        }
+        if (op.equals("+"))
+        {
+            result = nums.stream().reduce(0.0, (a, b) -> a + b);
+        }
+        else if (op.equals("-"))
+        {
+            result = nums.stream().reduce(0.0, (a, b) -> a - b);
+        }
+        else if (op.equals("*"))
+        {
+            result = nums.stream().reduce(0.0, (a, b) -> a * b);
+        }
+        else if (op.equals("/"))
+        {
+            result = nums.stream().reduce(0.0, (a, b) -> a / b);
+        }
+
+        return result;
     }
 }
